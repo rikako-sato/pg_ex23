e5acabfc404dffc3b66bc24f707a34b8b4ce77b1
z2160040 <z2160040@192.168.128.113>
2017-10-06 00:13:51 +0900

    微調整

--- service/料金計算システム/DayService.java
+++ service/料金計算システム/DayService.java
@@ -26,4 +26,43 @@ public class DayService extends TimeService {
 		return 0;
 	}
 
+	private boolean joined = false;
+
+	// サービスに加入しているかをチェック
+	public void checkService(String line) {
+		if (line.substring(2, 4).equals("E1")) {
+			joined = true;
+		}
+
+	}
+
+	public int calcBasicCharge(int baseBasicCharge) {
+		if (joined) {
+			// サービスに加入していれば基本料金を200円増し
+			return baseBasicCharge + 200;
+		}
+		return baseBasicCharge;
+	}
+
+	// 単価を計算する
+	public int calcUnitPrice(String line, int baseUnitPrice) {
+
+		if (!joined) {
+			// サービスに加入していなければ値引きなし
+			return baseUnitPrice;
+		}
+
+		int startHour = Integer.parseInt(line.substring(13, 15)); // 通話開始時間
+
+		if (8 <= startHour && startHour <= 17) {
+			// 通話開始時間がサービス対象時間ならば5円引き
+			return baseUnitPrice - 5;
+		}
+
+		return baseUnitPrice;
+	}
+
+	public void clear() {
+		joined = false;
+	}
 }
--- service/料金計算システム/DayServiceTest.java
+++ service/料金計算システム/DayServiceTest.java
@@ -1,6 +1,5 @@
 package 料金計算システム;
 
-
 public class DayServiceTest {
 
 		@Test
@@ -13,13 +12,37 @@ public class DayServiceTest {
 
 		}
 
+
+
 		@Test
-		public void testCalcUnitPrice() {
+		public void testCalcBasicCharge() {
+			DayService dayService = new DayService();
+
+			dayService.clear();
+			assertEquals(1000, dayService.calcBasicCharge(1000));
+
+			dayService.checkService("2 E1");
+			assertEquals(1200, dayService.calcBasicCharge(1000));
+		}
+
+		private void assertEquals(int i, int calcBasicCharge) {
+			// TODO 自動生成されたメソッド・スタブ
 
 		}
 
 		@Test
-		public void testCalcBasicCharge() {
+		public void testCalcUnitPrice() {
+			DayService dayService = new DayService();
+
+			dayService.clear();
+			int unitPrice = dayService.calcUnitPrice("5 2004/06/05 17:59 010 090-1234-0002", 20);
+			assertEquals(20, unitPrice);
+
+			dayService.checkService("2 E1");
+			unitPrice = dayService.calcUnitPrice("5 2004/06/05 18:00 010 090-1234-0002", 20);
+			assertEquals(20, unitPrice);
+			unitPrice = dayService.calcUnitPrice("5 2004/06/05 17:59 010 090-1234-0002", 20);
+			assertEquals(15, unitPrice);
 
 		}
 
